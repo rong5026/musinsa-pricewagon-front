87d604834d5b2bd5d98afee1655814b0a9e1e8e2
rong5026 <rong5026@naver.com>
2024-08-14 17:03:22 +0900

    init tailwind

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import './index.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className="text-3xl font-bold underline">
+      tailwind 적용
     
     </div>
   );
