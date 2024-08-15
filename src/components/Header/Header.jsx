6892fc4956f40699fa58ea71bf3d08bbffdf9b19
rong5026 <rong5026@naver.com>
2024-08-15 22:48:39 +0900

    feat 헤더

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import hamster from '../../assets/images/hamster.png'
 
 function Header() {
   return (
-    <header className="h-16 w-full bg-blue-500 text-white flex items-center justify-center z-10">
-      <h1 className="text-lg font-bold">Header</h1>
-    </header>
+    <header className="h-20 w-full bg-[#F1F4F6] z-10 flex items-center justify-between px-4">
+    <img alt='pricewagon' src={hamster} width={42} height={42} loading="lazy" />
+    <div className="flex flex-col justify-center pl-4">
+      <span className="font-bold leading-5">빠르고 간편하게 비교하기</span>
+      <span className="text-[12px] leading-4 text-gray-500">앱에서 더 쉽고 빠르게 이용해보세요!</span>
+    </div>
+    <div className="bg-green-400 text-white font-bold px-3 py-[6px] text-[12px] rounded-full">
+      <span>앱 다운로드</span>
+    </div>
+  </header>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        customBg : '#F1F4F6'
+      }
+    },
   },
   plugins: [],
 }
