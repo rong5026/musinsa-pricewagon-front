84d38d4dca5318a53ecfc4b8a141d4040a27d25e
rong5026 <rong5026@naver.com>
2024-08-16 15:09:59 +0900

    모바일 버전 로고 변경

--- src/components/Header/MobileHeader.jsx
+++ src/components/Header/MobileHeader.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import hamster from '../../assets/images/hamster.png';
+import Clover from '../../assets/images/clover.png'
 
 function MobileHeader() {
   return (
     <header className="h-20 w-full bg-[#F1F4F6] z-10 items-center justify-between px-4 flex header-hide">
       <img
         alt="pricewagon"
-        src={hamster}
+        src={Clover}
         width={42}
         height={42}
         loading="lazy"
@@ -17,7 +17,7 @@ function MobileHeader() {
           앱에서 더 쉽고 빠르게 이용해보세요!
         </span>
       </div>
-      <div className="bg-green-400 text-white font-bold px-3 py-[6px] text-[12px] rounded-full">
+      <div className="bg-green-500 text-white font-bold px-3 py-[6px] text-[12px] rounded-full">
         <span>앱 다운로드</span>
       </div>
     </header>
