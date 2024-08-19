daf08b4890f65b8d974bd34d76933a9bc9720938
rong5026 <rong5026@naver.com>
2024-08-19 18:18:35 +0900

    category layout

--- src/components/Header/Button/Category.jsx
+++ src/components/Header/Button/Category.jsx
@@ -2,7 +2,46 @@ import React from 'react'
 
 function Category() {
   return (
-    <div>Category</div>
+    <div className="md:px-8 2xl:px-8 lg:flex lg:h-16 items-center max-w-[1024px] hidden headerBottom mx-auto box-content">
+        <nav className="flex w-full relative lg:flex ps-3.5 xl:ps-5">
+            <div className='cursor-pointer'>
+                <a target="_self" className="relative inline-flex items-center px-3 py-3 text-sm font-normal xl:text-base text-heading xl:px-4 group-hover:text-black">
+                    상의
+                </a>
+            </div>
+            <div className='cursor-pointer'>
+                <a target="_self" className="relative inline-flex items-center px-3 py-3 text-sm font-normal xl:text-base text-heading xl:px-4 group-hover:text-black">
+                    아우터
+                </a>
+            </div>
+            <div className='cursor-pointer'>
+                <a target="_self" className="relative inline-flex items-center px-3 py-3 text-sm font-normal xl:text-base text-heading xl:px-4 group-hover:text-black">
+                    바지
+                </a>
+            </div>
+            <div className='cursor-pointer'>
+                <a target="_self" className="relative inline-flex items-center px-3 py-3 text-sm font-normal xl:text-base text-heading xl:px-4 group-hover:text-black">
+                    원피스/스커트
+                </a>
+            </div>
+            <div className='cursor-pointer'>
+                <a target="_self" className="relative inline-flex items-center px-3 py-3 text-sm font-normal xl:text-base text-heading xl:px-4 group-hover:text-black">
+                    신발
+                </a>
+            </div>
+            <div className='cursor-pointer'>
+                <a target="_self" className="relative inline-flex items-center px-3 py-3 text-sm font-normal xl:text-base text-heading xl:px-4 group-hover:text-black">
+                    가방
+                </a>
+            </div>
+            <div className='cursor-pointer'>
+                <a target="_self" className="relative inline-flex items-center px-3 py-3 text-sm font-normal xl:text-base text-heading xl:px-4 group-hover:text-black">
+                    패션소품
+                </a>
+            </div>
+            
+        </nav>
+    </div>
   )
 }
 
--- src/components/Header/CommonHeader.jsx
+++ src/components/Header/CommonHeader.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import MenuButton from './Button/Menu';
 import SearchButton from './Button/Search';
+import Category from './Button/Category';
 
 function CommonHeader() {
   return (
     <header className="px-4 w-full bg-slate-400 mt-20 sm:mt-0 sm:px-8 z-30 md:px-8 lg:px-6 h-16 sm:h-20 lg:h-36 xl:h-40 transition duration-200 ease-in-out border-b border-jnGray-300 fixed text-gray-700">
-      <div className="bg-white h-full lg:h-20 xl:h-24 px-0 lg:px-8 2xl:px- max-w-[1024px] flex items-center mx-auto box-content justify-between relative">
+      <div className="bg-white h-full 2xl:px-8 lg:h-20 xl:h-24 px-0 lg:px-8 max-w-[1024px] flex items-center mx-auto box-content justify-between relative">
         <MenuButton />
 
         <div className="flex text-center font-bold text-lg justify-center">
@@ -13,6 +14,8 @@ function CommonHeader() {
         </div>
         <SearchButton />
       </div>
+        <Category/>
+
     </header>
   );
 }
