eb9a32c66e71df7b88b661bf40ebd74dd0082d87
rong5026 <rong5026@naver.com>
2024-08-30 16:30:49 +0900

    home 화면으로 Link

--- src/components/Header/Title.jsx
+++ src/components/Header/Title.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 function Title() {
   return (
     <div className="bg-white flex text-center font-bold text-lg justify-center">
+      <Link
+        to={`/`} // 적절한 링크 경로 설정
+      >
       MUSINSAWAGON
+
+      </Link>
     </div>
   );
 }
