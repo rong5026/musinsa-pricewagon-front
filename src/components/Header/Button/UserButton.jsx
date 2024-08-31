f2e26a71027d878fe3cbeba39d067bfc3ab7c4d4
rong5026 <rong5026@naver.com>
2024-08-31 17:56:53 +0900

    상품 등록 활성화

--- src/components/Header/Button/UserButton.jsx
+++ src/components/Header/Button/UserButton.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
-const UserButton = ({ Icon, label }) => (
+const UserButton = ({ Icon, label, onClick }) => (
   <li className="flex items-center justify-center px-3 relative hover:bg-gray-100 transition-colors duration-300 rounded-full p-2">
-    <button className="group flex items-center justify-center whitespace-nowrap">
+    <button
+      className="group flex items-center justify-center whitespace-nowrap"
+      onClick={onClick}
+     
+    >
       <div className="relative cursor-pointer group-hover:scale-105 transition-transform duration-300">
         <Icon size={24} />
       </div>
@@ -13,6 +17,5 @@ const UserButton = ({ Icon, label }) => (
     <div className="absolute inset-y-1/2 -right-[1px] h-4 w-[1px] bg-gray-300 transform -translate-y-1/2" />
   </li>
 );
-
 
 export default UserButton;
--- src/components/Header/UserMenu.jsx
+++ src/components/Header/UserMenu.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
 import UserButton from './Button/UserButton';
 import { IoChatbubbleEllipsesOutline } from 'react-icons/io5';
 import { AiOutlineAppstoreAdd } from 'react-icons/ai';
 import { GoPerson } from 'react-icons/go';
+import ProductRegistrationModal from '../Modal/ProductRegistrationModal';
 
 const UserMenu = () => {
-    return (
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  return (
+    <>
       <div className="hidden lg:flex relative w-[300px]">
         <ul className="flex w-full text-sm font-medium list-none text-jnGray-900 break-keep">
           <UserButton Icon={IoChatbubbleEllipsesOutline} label="문의하기" />
-          <UserButton Icon={AiOutlineAppstoreAdd} label="상품 등록" />
+          <UserButton
+            Icon={AiOutlineAppstoreAdd}
+            label="상품 등록"
+            onClick={openModal}
+          />
           <UserButton Icon={GoPerson} label="마이페이지" />
         </ul>
       </div>
-    );
-  };
+      <ProductRegistrationModal isOpen={isModalOpen} onClose={closeModal} />
+    </>
+  );
+};
 
-export default UserMenu
+export default UserMenu;
