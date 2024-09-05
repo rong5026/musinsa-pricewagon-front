5e89e1a6e8002bebf7b699560ea1e5dd00923b36
rong5026 <rong5026@naver.com>
2024-09-05 20:12:40 +0900

    vercel product로 변경

--- src/components/Product/ProductDisplay.jsx
+++ src/components/Product/ProductDisplay.jsx
@@ -7,7 +7,7 @@ function ProductDisplay({products}) {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:pt-1 lg:grid-cols-4 gap-4">
       {products.map(product => (
-        <Product key={product.productNumber} product={product} />
+        <VercelProduct key={product.productNumber} product={product} />
       ))}
     </div>
   );
