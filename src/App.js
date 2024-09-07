2d8ca4c752659efdce941297b7d8d3408192b36e
rong5026 <rong5026@naver.com>
2024-09-07 17:44:11 +0900

    이름 오타 변경

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
 import MainPage from './containers/Main/MainPage';
 import Footer from './components/Footer/Footer';
-import ProductDeatil from './containers/ProductDetail/ProductDeatil';
+import ProductDetail from './containers/ProductDetail/ProductDetail';
 import ProductCategoryPage from './containers/ProductCategoryPage/ProductCategoryPage';
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
         <main className="flex-grow pt-36 pb-12 sm:pt-20 lg:pt-36 xl:pt-40 sm:px-6 max-w-6xl mx-auto">
           <Routes>
             <Route exact path="/" element={<MainPage />} />
-            <Route path="/:shoptype/products/:id" element={<ProductDeatil />} />
+            <Route path="/:shoptype/products/:id" element={<ProductDetail />} />
             <Route path="/:shoptype/category/:id" element={<ProductCategoryPage/>} />
             <Route path="/:shoptype/category/:id/sub_category/:sub_category_id" element={<ProductCategoryPage/>}/>
           </Routes>
